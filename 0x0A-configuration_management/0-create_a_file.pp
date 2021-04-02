c97c40228e4574844cc1b028858c8acc5558e461
ramsteven <ramsteven@gmail.com>
2021-04-02 17:07:55 +0000

    addign task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-file { '/tmp':
-	path => '/tmp/holberton'
-	mode => 0744
-	owner => www-data
-	group => ww-data
-	content => I love pupet
+# create puppet file
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love pupet'
 }
